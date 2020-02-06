05250c1d46d6105dfd303d3721516ebdec027871
shawnbmason <s.mason1993@yahoo.com>
2020-02-05 20:17:34 -0500

    updated

--- my_file.py
+++ my_file.py
@@ -1,16 +1,4 @@
 def hello_world():
     return "hello"
 
-
-def small_change():
-    print("This is a small change that I have done")
-
-def hi_there():
-    print("Updated python function with merge conflict demo")
-
-def faded():
-    print("You where the shadow to my light.... where are you now.... I'm faded....")
-
-def shawn_mason():
-    print("Was here")
-    
+hello_world()
